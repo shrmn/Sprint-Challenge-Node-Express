3da4dffbc84773648e0cf018f8faf4772a1731fe
Dan Sherman <dvsherman@protonmail.com>
2019-03-22 08:26:22 -0700

    added nodemon, created scripts start & server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+const server = require('./server');
 
-// code away!
+server.listen(6500, () => {
+  console.log(`\n *** SERVER RUNNING ON http://localhost:6500 ***\n`)
+});
