cceb7315e64e12897ec7df56e191fe3af7cc0c43
Dan Sherman <dvsherman@protonmail.com>
2019-03-22 08:37:27 -0700

    created file/folder structure, building out server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+
+const projectRouter = require('./routers/projectRouter.js');
+const actionRouter = require('./routers/actionRouter.js');
+
+const server = express();
+
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Node/Express Sprint Challenge</h2>
+    <p>Server is running!</p>
+    `)
+});
+
+module.exports = server;
